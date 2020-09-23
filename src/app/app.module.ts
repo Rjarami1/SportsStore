0dfa2e41d62e23202870cbe79d448b60c86c89d6
Ricardo Jaramillo <rjarami1@eafit.edu.co>
2020-09-23 09:55:54 -0500

    added files

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { StoreModule } from './store/store.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, StoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/model/product.repository.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { Product } from "./product.model";
+import { StaticDataSource } from "./static.datasource";
+
+@Injectable()
+export class ProductRepository{
+    private products: Product[] = [];
+    private categories: string[] = [];
+
+    constructor(private dataSource: StaticDataSource){
+        dataSource.getProducts().subscribe(data => {
+            this.products = data;
+            this.categories = data.map(p => p.category).filter((c, index, array) => array.indexOf(c) == index).sort();
+        });
+    }
+
+    getProducts(category: string = null): Product[]{
+        return this.products.filter(product => category == null || product.category == category);
+    }
+
+    getProduct(id: number): Product{
+        return this.products.find(product => product.id == id);
+    }
+
+    getCategories(): string[]{
+        return this.categories;
+    }
+}
