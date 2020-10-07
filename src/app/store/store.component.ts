21a3cee912d0fb8d8b6d1933494e4c4c9a475d53
Ricardo Jaramillo <Ricardo.Jaramillo@o-i.com>
2020-10-07 10:01:46 -0500

    Creation of directive for pagination.

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,12 +8,15 @@ import { ProductRepository } from "../model/product.repository";
 })
 export class StoreComponent{
 
-    public selectedCategory: string = null; 
+    public selectedCategory: string = null;
+    public productsPerPage: number = 4;
+    public selectedPage: number = 1; 
 
     constructor(private repository: ProductRepository){}
 
     get products(): Product[] {
-        return this.repository.getProducts(this.selectedCategory);
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        return this.repository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     get categories(): string[] {
@@ -23,4 +26,21 @@ export class StoreComponent{
     changeCategory(newCategory?:string){
         this.selectedCategory = newCategory;
     }
+
+    changePage(newPage: number){
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize: number){
+        this.productsPerPage = Number(newSize);
+        this.changePage(1);
+    }
+
+    get pageCount(): number{
+        return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage);
+    }
+
+    // get pageNumbers(): number[]{
+    //     return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)).fill(0).map((x,i) => i + 1);
+    // }
 }
